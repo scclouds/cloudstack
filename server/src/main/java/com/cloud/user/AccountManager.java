978329b8e06efeb410dd1bf9bda9c384c73cc7aa
Henrique Sato <henrique.sato@scclouds.com.br>
2024-01-22 12:25:21 -0300

    Limitar visibilidade na API `listRoles`

--- server/src/main/java/com/cloud/user/AccountManager.java
+++ server/src/main/java/com/cloud/user/AccountManager.java
@@ -195,7 +195,10 @@ public interface AccountManager extends AccountService, Configurable {
     UserTwoFactorAuthenticator getUserTwoFactorAuthenticator(final Long domainId, final Long userAccountId);
 
     void verifyUsingTwoFactorAuthenticationCode(String code, Long domainId, Long userAccountId);
+
     UserTwoFactorAuthenticationSetupResponse setupUserTwoFactorAuthentication(SetupUserTwoFactorAuthenticationCmd cmd);
 
     void checkApiAccess(Account caller, String command);
+
+    List<String> getApiNameList();
 }
