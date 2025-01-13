14d44d45815c6fb818bca1a2f129ebe5337d21e5
Fabrício Duarte <fabricio.duarte@scclouds.com.br>
2025-01-13 16:42:18 -0300

    Port 4.20 - Mapeamento de controladoras de disco

--- plugins/hypervisors/vmware/src/main/java/com/cloud/hypervisor/guru/VmwareVmImplementer.java
+++ plugins/hypervisors/vmware/src/main/java/com/cloud/hypervisor/guru/VmwareVmImplementer.java
@@ -323,7 +323,7 @@ class VmwareVmImplementer {
         }
 
         if (vm.getType() == VirtualMachine.Type.NetScalerVm) {
-            details.put(VmDetailConstants.ROOT_DISK_CONTROLLER, "scsi");
+            details.put(VmDetailConstants.ROOT_DISK_CONTROLLER, DiskControllerType.scsi.toString());
         }
     }
 
