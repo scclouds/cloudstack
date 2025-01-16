763fa3349557b5f814393cd11d04a2fce7a3e3a6
Henrique Sato <henrique.sato@scclouds.com.br>
2025-01-15 22:12:37 -0300

    Remover migração das keypairs do arquivo java de upgrade da 4.20.0.0 pra 4.20.0.1

--- engine/schema/src/main/java/com/cloud/upgrade/dao/Upgrade42000to42001.java
+++ engine/schema/src/main/java/com/cloud/upgrade/dao/Upgrade42000to42001.java
@@ -21,14 +21,11 @@ import org.apache.cloudstack.engine.subsystem.api.storage.SnapshotInfo;
 
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class Upgrade42000to42001 extends DbUpgradeAbstractImpl {
 
@@ -94,13 +91,6 @@ public class Upgrade42000to42001 extends DbUpgradeAbstractImpl {
                 throw new CloudRuntimeException(message, e);
             }
         }
-
-        try {
-            performKeyPairMigration(conn);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
     }
 
     @Override
@@ -113,43 +103,4 @@ public class Upgrade42000to42001 extends DbUpgradeAbstractImpl {
 
         return new InputStream[] {script};
     }
-
-    private void performKeyPairMigration(Connection conn) throws SQLException {
-        try {
-            logger.debug("Performing keypair migration from user table to api_keypair table.");
-            PreparedStatement pstmt = conn.prepareStatement("SELECT u.id, u.api_key, u.secret_key, a.domain_id, a.id FROM `cloud`.`user` AS u JOIN `cloud`.`account` AS a " +
-                    "ON u.account_id = a.id WHERE u.api_key IS NOT NULL AND u.secret_key IS NOT NULL");
-            ResultSet resultSet = pstmt.executeQuery();
-
-            while (resultSet.next()) {
-                long id = resultSet.getLong(1);
-                String apiKey = resultSet.getString(2);
-                String secretKey = resultSet.getString(3);
-                Long domainId = resultSet.getLong(4);
-                Long accountId = resultSet.getLong(5);
-                Date timestamp = Date.valueOf(LocalDate.now());
-
-                PreparedStatement preparedStatement = conn.prepareStatement("INSERT IGNORE INTO `cloud`.`api_keypair` (uuid, user_id, domain_id, account_id, api_key, secret_key, created, name) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-                String uuid = UUID.randomUUID().toString();
-                preparedStatement.setString(1, uuid);
-                preparedStatement.setLong(2, id);
-                preparedStatement.setLong(3, domainId);
-                preparedStatement.setLong(4, accountId);
-
-                preparedStatement.setString(5, apiKey);
-                preparedStatement.setString(6, secretKey);
-                preparedStatement.setDate(7, timestamp);
-                preparedStatement.setString(8, uuid);
-
-                preparedStatement.executeUpdate();
-            }
-            pstmt = conn.prepareStatement("ALTER TABLE `cloud`.`user` DROP COLUMN IF EXISTS api_key, DROP COLUMN IF EXISTS secret_key;");
-            pstmt.executeUpdate();
-            logger.info("Successfully performed keypair migration.");
-        } catch (SQLException ex) {
-            logger.info("Unexpected exception in user keypair migration", ex);
-            throw ex;
-        }
-    }
-
 }
