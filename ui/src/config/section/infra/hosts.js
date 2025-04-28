508af8f7b406e69fb43fb97d16a28a7c93497b30
Layon Jordão Ribeiro e Oliveira <layon.oliveira@scclouds.com.br>
2025-04-28 13:11:16 -0300

    Adição da quantidade de _system_ VMs à listagem de _hosts_ na GUI

--- ui/src/config/section/infra/hosts.js
+++ ui/src/config/section/infra/hosts.js
@@ -32,7 +32,11 @@ export default {
   },
   params: { type: 'routing' },
   columns: () => {
-    const fields = ['name', 'state', 'resourcestate', 'ipaddress', 'hypervisor', 'instances', 'powerstate', 'version']
+    const fields = [
+      'name', 'state', 'resourcestate', 'ipaddress',
+      'hypervisor', 'instances', { field: 'systeminstances', customTitle: 'system.vms' },
+      'powerstate', 'version'
+    ]
     const metricsFields = ['cpunumber', 'cputotalghz', 'cpuusedghz', 'cpuallocatedghz', 'memorytotalgb', 'memoryusedgb', 'memoryallocatedgb', 'networkread', 'networkwrite']
     if (store.getters.metrics) {
       fields.push(...metricsFields)
