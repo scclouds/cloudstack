f80e5fac52f2c8bec612e2a1c841441e706cc120
Fabricio Duarte <fabricio.duarte@scclouds.com.br>
2025-08-12 15:45:50 -0300

    Change version to 4.20.0.5-scclouds

--- engine/schema/src/main/java/com/cloud/upgrade/DatabaseUpgradeChecker.java
+++ engine/schema/src/main/java/com/cloud/upgrade/DatabaseUpgradeChecker.java
@@ -38,6 +38,7 @@ import com.cloud.upgrade.dao.Upgrade42000to42001;
 import com.cloud.upgrade.dao.Upgrade42001to42002;
 import com.cloud.upgrade.dao.Upgrade42002to42003;
 import com.cloud.upgrade.dao.Upgrade42003to42004;
+import com.cloud.upgrade.dao.Upgrade42004to42005;
 import com.cloud.utils.FileUtil;
 import org.apache.cloudstack.utils.CloudStackVersion;
 import org.apache.commons.lang3.StringUtils;
@@ -240,6 +241,7 @@ public class DatabaseUpgradeChecker implements SystemIntegrityChecker {
                 .next("4.20.0.1", new Upgrade42001to42002())
                 .next("4.20.0.2", new Upgrade42002to42003())
                 .next("4.20.0.3", new Upgrade42003to42004())
+                .next("4.20.0.4", new Upgrade42004to42005())
                 .build();
     }
 
