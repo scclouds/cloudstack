dbc408030394bbd49ee17522d0ceb7431118a682
Bernardo De Marco Gonçalves <bernardo.goncalves@scclouds.com.br>
2025-09-18 13:19:33 -0300

    Correção de carregamento infinito nos filtros da página de volumes

--- ui/src/components/view/SearchView.vue
+++ ui/src/components/view/SearchView.vue
@@ -947,7 +947,7 @@ export default {
     },
     fetchVolumes (searchKeyword) {
       return new Promise((resolve, reject) => {
-        api('listvolumes', { listAll: true, isencrypted: searchKeyword }).then(json => {
+        api('listVolumes', { listAll: true, isencrypted: searchKeyword }).then(json => {
           const volumes = json.listvolumesresponse.volume
           resolve({
             type: 'isencrypted',
