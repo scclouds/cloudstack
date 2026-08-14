b5cc9ad1a1fc7cdf7efc3340672cc7dc858317be
Erik Böck <erik.bock@outlook.com>
2026-08-14 13:51:59 -0300

    Create device offering tables and fix unused import

--- engine/schema/src/main/resources/META-INF/db/schema-42210to42300.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-42210to42300.sql
@@ -646,3 +646,72 @@ CALL `cloud`.`IDEMPOTENT_ADD_COLUMN`('cloud.backup_schedule', 'isolated', 'TINYI
 
 UPDATE `cloud`.`configuration` SET `value`=CONCAT(`value`, ', backupValidationCommandTimeout, backupValidationScreenshotWait, backupValidationBootTimeout')
 WHERE `name`='user.vm.readonly.details' AND `value` IS NOT NULL;
+
+-- KVM host devices table
+
+CREATE TABLE IF NOT EXISTS `cloud`.`host_pci_devices` (
+  `id` bigint unsigned NOT NULL auto_increment COMMENT 'Row ID',
+  `uuid` varchar(255) NOT NULL COMMENT 'UUID device UUID',
+  `display_name` varchar(255) NOT NULL COMMENT 'Device human readable name',
+  `pci_name` varchar(255) NOT NULL COMMENT 'Device unique identifier',
+  `pci_class` varchar(255) NOT NULL COMMENT 'Device class',
+  `pci_domain` varchar(255) NOT NULL COMMENT 'Device domain',
+  `pci_bus` varchar(255) NOT NULL COMMENT 'Device bus',
+  `pci_slot` varchar(255) NOT NULL COMMENT 'Device slot',
+  `pci_function` varchar(255) NOT NULL COMMENT 'Device function',
+  `pci_vendor_id` varchar(255) NOT NULL COMMENT 'Device vendor identifier',
+  `pci_device_id` varchar(255) NOT NULL COMMENT 'Device identifier, set by its vendor',
+  `device_tag` varchar(255) NOT NULL COMMENT 'Device tag, used to assign it to devices offerings',
+  `created` datetime NOT NULL COMMENT 'Device creation timestamp',
+  `removed` datetime DEFAULT NULL COMMENT 'Device removal timestamp',
+  `state` varchar(255) NOT NULL COMMENT 'Device state',
+  `type` varchar(255) NOT NULL COMMENT 'Devices type, based on its class',
+  `instance_id` bigint unsigned DEFAULT NULL COMMENT 'Device allocator instance id. Foreign key that points to the vm_instance table',
+  `account_id` bigint unsigned DEFAULT NULL COMMENT 'Device allocator account id. Foreign key that points to the account table',
+  `host_id` bigint unsigned DEFAULT NULL COMMENT 'Device host id. Foreign key that points to the host table',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_host_pci_devices_instance_id` FOREIGN KEY (`instance_id`) REFERENCES `vm_instance` (`id`),
+  CONSTRAINT `fk_host_pci_devices_account_id` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`),
+  CONSTRAINT `fk_host_pci_devices_host_id` FOREIGN KEY (`host_id`) REFERENCES `host` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Device offerings table
+
+CREATE TABLE IF NOT EXISTS `cloud`.`device_offerings` (
+  `id` bigint unsigned NOT NULL auto_increment COMMENT 'Row ID',
+  `uuid` varchar(255) NOT NULL COMMENT 'Device offering UUID',
+  `name` varchar(255) NOT NULL COMMENT 'Device offering name',
+  `description` varchar(255) NOT NULL COMMENT 'Device offering description',
+  `state` varchar(255) NOT NULL COMMENT 'Device offering state',
+  `created` datetime NOT NULL COMMENT 'Device offering creation timestamp',
+  `removed` datetime DEFAULT NULL COMMENT 'Device offering removal timestamp',
+  `account` bigint unsigned NOT NULL COMMENT 'Device offering creator account id. Foreign key that points to the account table',
+  `public` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Whether the offering is available for all users or not. Will always be false if the domain_id attribute is set',
+  `domain_id` bigint unsigned DEFAULT NULL COMMENT 'The domain that this offering will be available to. Foreign key that points to the domain table',
+  `zone_id` bigint unsigned DEFAULT NULL COMMENT 'The zone that this offering will be available to. Foreign key that points to the data_center table',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_device_offerings_account` FOREIGN KEY (`account`) REFERENCES `account` (`id`),
+  CONSTRAINT `fk_device_offerings_domain_id` FOREIGN KEY (`domain_id`) REFERENCES `domain` (`id`),
+  CONSTRAINT `fk_device_offerings_zone_id` FOREIGN KEY (`zone_id`) REFERENCES `data_center` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Device offerings device tags mapping table
+
+CREATE TABLE IF NOT EXISTS `cloud`.`device_offering_device_tags` (
+  `id` bigint unsigned NOT NULL auto_increment COMMENT 'Row ID',
+  `device_offering_id` bigint unsigned NOT NULL COMMENT 'Device offering id. Foreign key that points to the device_offerings table',
+  `device_tag` varchar(255) NOT NULL COMMENT 'Device tag, used to assign it to devices offerings',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_device_offering_device_tags_device_offering_id` FOREIGN KEY (`device_offering_id`) REFERENCES `device_offerings` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- VM Instance device offering mapping table
+
+CREATE TABLE IF NOT EXISTS `cloud`.`vm_instance_device_offerings` (
+    `id` bigint unsigned NOT NULL auto_increment COMMENT 'Row ID',
+    `vm_instance_id` bigint unsigned NOT NULL COMMENT 'VM instance id. Foreign key that points to the vm_instance table',
+    `device_offering_id` bigint unsigned NOT NULL COMMENT 'Device offering id. Foreign key that points to the device_offerings table',
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_vm_instance_device_offerings_vm_instance_id` FOREIGN KEY (`vm_instance_id`) REFERENCES `vm_instance` (`id`),
+    CONSTRAINT `fk_vm_instance_device_offerings_device_offering_id` FOREIGN KEY (`device_offering_id`) REFERENCES `device_offerings` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- plugins/hypervisors/kvm/src/main/java/org/apache/cloudstack/kvm/libvirt/mappers/serialization/LibvirtDeviceDeserializer.java
+++ plugins/hypervisors/kvm/src/main/java/org/apache/cloudstack/kvm/libvirt/mappers/serialization/LibvirtDeviceDeserializer.java
@@ -1,6 +1,5 @@
 package org.apache.cloudstack.kvm.libvirt.mappers.serialization;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
